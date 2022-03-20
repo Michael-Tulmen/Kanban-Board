1126fb2698da1b4083c18b4a5f1728580c12f944
Michael Tulmen <michaeltulmen@gmail.com>
2022-03-19 20:42:39 -0500

    event listener for task handler

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,13 @@
-var formEl = document.querySelector("#task-form");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function(event) {
-    event.preventDefault();
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = 'task-item';
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
