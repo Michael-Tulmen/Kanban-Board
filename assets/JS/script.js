1ca372249e697ac28d1159ed76de903e11ab973e
Michael Tulmen <michaeltulmen@gmail.com>
2022-03-19 20:28:01 -0500

    adding style files to branch

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,11 @@
-var buttonEl  = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function () {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task.";
+    listItemEl.className = 'task-item';
+    listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-});
+}
